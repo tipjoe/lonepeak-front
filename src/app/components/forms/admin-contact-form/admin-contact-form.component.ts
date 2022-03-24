76e8e4e90caae45627fde8bfe3701232c374bce3
Jake P <pedigojacob@gmail.com>
2022-03-24 15:42:43 -0600

    corrections to registration-form and admin-contact-form templateUrls and styleUrls

--- src/app/components/forms/admin-contact-form/admin-contact-form.component.ts
+++ src/app/components/forms/admin-contact-form/admin-contact-form.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-admin-contact-form',
-  templateUrl: './admin-contact.component.html',
-  styleUrls: ['./admin-contact.component.sass'],
+  templateUrl: './admin-contact-form.component.html',
+  styleUrls: ['./admin-contact-form.component.sass'],
 })
 export class AdminContactComponent implements OnInit {
   constructor() {}
--- src/app/components/forms/registration-form/registration-form.component.ts
+++ src/app/components/forms/registration-form/registration-form.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-registration-form',
-  templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.sass'],
+  templateUrl: './registration-form.component.html',
+  styleUrls: ['./registration-form.component.sass'],
 })
 export class RegistrationComponent implements OnInit {
   constructor() {}
