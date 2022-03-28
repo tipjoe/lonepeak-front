67aec2c2bd1c61696b67448e3659d21eddf2204f
Jake P <pedigojacob@gmail.com>
2022-03-28 16:16:29 -0600

    added two way databinding to sliders

--- src/app/components/forms/questions-form/questions-form.component.ts
+++ src/app/components/forms/questions-form/questions-form.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-questions-form',
   templateUrl: './questions-form.component.html',
-  styleUrls: ['./questions-form.component.sass']
+  styleUrls: ['./questions-form.component.sass'],
 })
 export class QuestionsFormComponent implements OnInit {
+  extroIntro: number | null = 50;
+  prepValue: number | null = 50;
+  pronounsValue: number | null = 2;
+  touchyTouchy: number | null = 50;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  getExtroValue() {
+    return this.extroIntro;
   }
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
