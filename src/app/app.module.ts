ec01f5a2736736ccce08e6e2a83c622a7899ec87
Jake P <pedigojacob@gmail.com>
2022-04-11 15:56:32 -0600

    corrections to stepper implementation and mask bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -86,7 +86,7 @@ import { RoadState } from './store/road/road.state';
 import { UserState } from './store/user/user.state';
 
 // Ngx-Mask Config
-export const options: Partial<IConfig> | (() => Partial<IConfig>) = null;
+// export const options: Partial<IConfig> | (() => Partial<IConfig>) = null;
 
 // Internal Services
 // App-based services are made available globally with the
